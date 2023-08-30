6b08e853ced42ccb2ee680bc40edc55308a0807b
JIAN11442 <yikjian11442@gmail.com>
2023-08-30 12:01:33 +0800

    DONE: Merge Two Sorted Lists

--- 21_Merge_Two_Sorted_Lists.js
+++ 21_Merge_Two_Sorted_Lists.js
@@ -5,7 +5,26 @@ class ListNode {
   }
 }
 
-const mergeTwoLists = (list1, list2) => {};
+const mergeTwoLists = (l1, l2) => {
+  let list = new ListNode(0);
+  let head = list;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      head.next = l1;
+      l1 = l1.next;
+    } else {
+      head.next = l2;
+      l2 = l2.next;
+    }
+
+    head = head.next;
+  }
+  head.next = l1 || l2;
+
+  console.log(list.next);
+  return list.next;
+};
 
 mergeTwoLists(
   new ListNode(1, new ListNode(2, new ListNode(4))),
