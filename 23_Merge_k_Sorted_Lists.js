019b91a5ab2f2eff27284c48228d7b4dcc856870
JIAN11442 <yikjian11442@gmail.com>
2023-08-31 11:50:11 +0800

    DONE: Merge K Sorted Lists

--- 23_Merge_k_Sorted_Lists.js
+++ 23_Merge_k_Sorted_Lists.js
@@ -6,7 +6,41 @@ class ListNode {
 }
 
 const mergeKLists = (lists) => {
-  console.log(lists);
+  let res = null;
+  let reslist = [];
+  const twoMerge = (l1, l2) => {
+    let list = new ListNode(0);
+    let head = list;
+    let result = [];
+
+    if (!l1) return l2;
+    if (!l2) return l1;
+
+    while (l1 && l2) {
+      if (l1.val < l2.val) {
+        head.next = l1;
+        l1 = l1.next;
+      } else {
+        head.next = l2;
+        l2 = l2.next;
+      }
+      head = head.next;
+    }
+    head.next = l1 || l2;
+
+    // console.log(list.next);
+    return list.next;
+  };
+
+  for (let i = 0; i < lists.length; i++) {
+    res = twoMerge(res, lists[i]);
+  }
+  while (res) {
+    reslist.push(res.val);
+    res = res.next;
+  }
+  console.log(reslist);
+  // console.log(res);
 };
 
 mergeKLists([
@@ -15,5 +49,5 @@ mergeKLists([
   new ListNode(2, new ListNode(6)),
 ]);
 
-// mergeKLists(new ListNode(null));
-// mergeKLists(new ListNode(new ListNode(null)));
+mergeKLists(new ListNode(null));
+mergeKLists(new ListNode(new ListNode(null)));
