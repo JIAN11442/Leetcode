274503adc7a069f1db479bec0c9b38bac0bab834
JIAN11442 <yikjian11442@gmail.com>
2023-09-01 23:52:12 +0800

    DONE: Swap Nodes in Pairs

--- 24_Swap_Nodes_in_Pairs.js
+++ 24_Swap_Nodes_in_Pairs.js
@@ -6,7 +6,22 @@ class ListNode {
 }
 
 const swapPairs = (head) => {
-  console.log(head);
+  let dummy = new ListNode(-1, head);
+  let curr = dummy;
+
+  while (curr.next !== null && curr.next.next !== null) {
+    let p1 = curr.next;
+    let p2 = curr.next.next.next;
+
+    curr.next = curr.next.next;
+    curr.next.next = p1;
+    p1.next = p2;
+
+    curr = curr.next.next;
+  }
+
+  console.log(dummy.next);
+  return dummy.next;
 };
 
 swapPairs(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))));
