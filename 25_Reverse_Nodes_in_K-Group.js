0e6ecb3e207eae7624c6d528fc1a75127fcfe277
JIAN11442 <yikjian11442@gmail.com>
2023-09-03 15:56:25 +0800

    DONE: Reverse Nodes in K-Group

---
+++ 25_Reverse_Nodes_in_K-Group.js
@@ -0,0 +1,41 @@
+class ListNode {
+  constructor(val, next = null) {
+    (this.val = val), (this.next = next);
+  }
+}
+
+const reverseKGroup = (head, k) => {
+  const recurs = (h) => {
+    let curr = h;
+    let count = 0;
+
+    while (curr && count < k) {
+      curr = curr.next;
+      count++;
+    }
+
+    if (count == k) {
+      curr = recurs(curr);
+
+      while (count > 0) {
+        let tmp = h.next;
+        h.next = curr;
+        curr = h;
+        h = tmp;
+
+        count--;
+      }
+      h = curr;
+    }
+    return h;
+  };
+  return recurs(head);
+};
+
+reverseGroup(
+  new ListNode(
+    1,
+    new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
+  ),
+  3
+);
