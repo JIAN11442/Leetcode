320e7d8a712681e20c1f737743749429a5ab7e6c
JIAN11442 <yikjian11442@gmail.com>
2023-09-08 16:57:10 +0800

    DONE: Rotate Array

--- 189_Rotate_Array.js
+++ 189_Rotate_Array.js
@@ -1,3 +1,4 @@
+// // Solution 1：沒效率(超出運行時間)：
 // const rotate = (nums, k) => {
 //   let count = 0;
 //   while (count < k) {
@@ -12,15 +13,54 @@
 //   return nums;
 // };
 
+// // Solution 2：沒效率(超出運行時間)
+// const rotate = (nums, k) => {
+//   let count = 0;
+//   while (count < k) {
+//     let j = nums.pop();
+//     nums.splice(0, 0, j);
+//     count++;
+//   }
+//   return nums;
+// };
+
+// Solution 3 : 相對有效率『TC : O(N); SC : O(1)』
+// const rotate = (nums, k) => {
+//   for (let i = nums.length - 1; i >= 0; i--) {
+//     nums[i + k] = nums[i];
+//   }
+
+//   for (let j = k - 1; j >= 0; j--) {
+//     nums[j] = nums.pop();
+//   }
+
+//   return nums;
+// };
+
+// Solution 4 : 相對有效率『TC: O(N); SC : O(1)』
+
 const rotate = (nums, k) => {
-  let count = 0;
-  while (count < k) {
-    let j = nums.pop();
-    nums.splice(0, 0, j);
-    count++;
-  }
+  const reverse = (i, j) => {
+    while (i < j) {
+      let temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+      i++;
+      j--;
+    }
+  };
+
+  k %= nums.length;
+
+  reverse(0, nums.length - 1);
+  reverse(0, k - 1);
+  reverse(k, nums.length - 1);
+
+  console.log(nums);
   return nums;
 };
 
+rotate([-1], 2);
+rotate([1, 2, 3, 4, 5], 6);
 // rotate([1, 2, 3, 4, 5, 6, 7], 3);
 // rotate([-1, -100, 3, 99], 2);
