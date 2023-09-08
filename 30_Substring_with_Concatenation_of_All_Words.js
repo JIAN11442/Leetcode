5da2f207f0720f5130fb1f7461aa9b7bce111beb
JIAN11442 <yikjian11442@gmail.com>
2023-09-08 17:09:22 +0800

    UNDONE: Substring with Concatenation of All Words

---
+++ 30_Substring_with_Concatenation_of_All_Words.js
@@ -0,0 +1,5 @@
+const findSubstring = (s, words) => {};
+
+findSubstring("barfoothefoobarman", ["foo", "bar"]);
+findSubstring("wordgoodgoodgoodbestword", ["word", "good", "best", "word"]);
+findSubstring("barfoofoobarthefoobarman", ["bar", "foo", "the"]);
